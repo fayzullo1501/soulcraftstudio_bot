1928767438976b5eef76ed6353f6f93983254769
Fayzullo <92411568+fayzullo1501@users.noreply.github.com>
2023-10-24 16:34:40 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
+// 6647221838:AAFxmmP7u5K2MsjlfHWoRsRjNe_5gUmLgX0   6498144305
+
 const TelegramBot = require('node-telegram-bot-api');
-const token = '6647221838:AAFxmmP7u5K2MsjlfHWoRsRjNe_5gUmLgX0'; // Замените на ваш токен
+const token = '6647221838:AAFxmmP7u5K2MsjlfHWoRsRjNe_5gUmLgX0'; // Replace with your token
 const bot = new TelegramBot(token, { polling: true });
 
 const serviceKeyboard = [
@@ -8,7 +10,7 @@ const serviceKeyboard = [
 ];
 
 const userData = {};
-let requestCounter = 1; // Переменная для хранения номера заявки
+let requestCounter = 1;
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
@@ -33,6 +35,7 @@ bot.on('text', (msg) => {
       topic: '',
       language: '',
       pages: 0,
+      deadline: '', // Add a deadline field
       name: '',
       contact: '',
       address: '',
@@ -55,10 +58,13 @@ bot.on('text', (msg) => {
   } else if (!userData[userId].pages) {
     if (!isNaN(messageText)) {
       userData[userId].pages = parseInt(messageText);
-      bot.sendMessage(chatId, 'Ism Familyangizni kiriting:');
+      bot.sendMessage(chatId, 'Dedlayn kunini kiriting:'); // Prompt for the deadline date
     } else {
       bot.sendMessage(chatId, 'Taqdimotning yaroqli necha varoqligini kiriting:');
     }
+  } else if (!userData[userId].deadline) {
+    userData[userId].deadline = messageText;
+    bot.sendMessage(chatId, 'Ism Familyangizni kiriting:');
   } else if (!userData[userId].name) {
     userData[userId].name = messageText;
     bot.sendMessage(chatId, 'Telefon raqamingizni kiriting:');
@@ -67,21 +73,22 @@ bot.on('text', (msg) => {
 
     if (msg.from.username) {
       userData[userId].address = `@${msg.from.username}`;
-      const adminChatId = '6498144305'; // Замените на chat_id администратора
-      const userMessage = `Buyurtma № ${requestCounter}:\n\nServis: ${userData[userId].service}\nMavzu: ${userData[userId].topic}\nTil: ${userData[userId].language}\nVaroqlar soni: ${userData[userId].pages}\nFIO: ${userData[userId].name}\nTelefon raqami: ${userData[userId].contact}\nTelegram nik (adres): ${userData[userId].address}`;
+      const adminChatId = '6498144305'; // Replace with the admin's chat_id
+      const userMessage = `Buyurtma № ${requestCounter}:\n\nServis: ${userData[userId].service}\nMavzu: ${userData[userId].topic}\nTil: ${userData[userId].language}\nVaroqlar soni: ${userData[userId].pages}\nDedlayn: ${userData[userId].deadline}\nFIO: ${userData[userId].name}\nTelefon raqami: ${userData[userId].contact}\nTelegram nik (adres): ${userData[userId].address}`;
       bot.sendMessage(adminChatId, userMessage);
       bot.sendMessage(chatId, 'Buyurtmangiz muvoffaqiyatli yuborildi. Operator javobini kuting!');
-      requestCounter++; // Увеличиваем номер заявки
+      requestCounter++;
     } else {
-      const adminChatId = '6498144305'; // Замените на chat_id администратора
-      const userMessage = `Buyurtma № ${requestCounter}:\n\nServis: ${userData[userId].service}\nMavzu: ${userData[userId].topic}\nTil: ${userData[userId].language}\nVaroqlar soni: ${userData[userId].pages}\nFIO: ${userData[userId].name}\nTelefon raqami: ${userData[userId].contact}`;
+      const adminChatId = '6498144305'; // Replace with the admin's chat_id
+      const userMessage = `Buyurtma № ${requestCounter}:\n\nServis: ${userData[userId].service}\nMavzu: ${userData[userId].topic}\nTil: ${userData[userId].language}\nVaroqlar soni: ${userData[userId].pages}\nDedlayn: ${userData[userId].deadline}\nFIO: ${userData[userId].name}\nTelefon raqami: ${userData[userId].contact}`;
       bot.sendMessage(adminChatId, userMessage);
       bot.sendMessage(chatId, 'Buyurtmangiz muvoffaqiyatli yuborildi. Operator javobini kuting!');
-      requestCounter++; // Увеличиваем номер заявки
+      requestCounter++;
     }
 
     delete userData[userId];
   }
 });
 
-module.exports = bot; // Экспортируем бота
+module.exports = bot;
+
